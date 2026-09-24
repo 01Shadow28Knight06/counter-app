ce4846854fd42be569abd0c648ef53f33df3e0e7
Sachk <sachk2012@gmail.com>
2026-09-24 11:16:56 -0600

    Initial commit: my first working counter app

---
+++ script.js
@@ -0,0 +1,11 @@
+let count = 0;
+
+function increase() {
+    count = count + 1;
+    document.getElementById("counterValue").innerText = count;
+}
+
+function decrease() {
+    count = count - 1;
+    document.getElementById("counterValue").innerText = count;
+}
